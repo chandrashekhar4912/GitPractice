4e0ae91a97bb31d8ca475a2c85c533eebe26f667
chandrashekhar4912 <chandrashekhar4912@gmail.com>
2024-12-22 01:20:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import boto3
 client = boto3.client('ec2')
 response = client.run_instances(
-    ImageId='ami-0453ec754f44f9a4a',
+    ImageId='string',
     InstanceType='t2.micro',
-    KeyName='cp',
-    MaxCount=1,
-    MinCount=1,
+    KeyName='string',
+    MaxCount=0,
+    MinCount=0,
 )
