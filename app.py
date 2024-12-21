19680fd0bc518a76b911ccec3b077cbabd400029
Chandrashekhar4912 <chandrashekhar4912@gmail.com>
2024-12-21 15:26:26 +0530

    adding app.py file in local repository

---
+++ app.py
@@ -0,0 +1,9 @@
+import boto3
+client = boto3.client('ec2')
+response = client.run_instances(
+    ImageId='ami-0453ec754f44f9a4a',
+    InstanceType='t2.micro',
+    KeyName='cp',
+    MaxCount=1,
+    MinCount=1,
+)
